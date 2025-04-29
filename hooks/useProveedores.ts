bd4df876e42e6978efb2228780aaa5abed345847
rogelionufi <rogelio@nufi.mx>
2025-04-29 09:17:33 -0600

    Agregar desactivación de la regla exhaustiva de dependencias en el hook useProveedores para evitar advertencias innecesarias en el desarrollo. Esto mejora la flexibilidad del hook sin comprometer su funcionalidad.

--- hooks/useProveedores.ts
+++ hooks/useProveedores.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 import { supabase } from '@/lib/supabase'
 import { Proveedor, Categoria, ProveedorDetalle, Documento, TipoDocumento, Transaccion } from '@/types/supabase'
